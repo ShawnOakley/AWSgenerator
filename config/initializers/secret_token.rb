7d3c075dbe9e7c1438f0d85be1255c3f935e5133
Shawn Oakley <Shawn Oakley>
2014-09-13 15:10:56 -0400

    Adds aws-sdk gem.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AWSgenerator::Application.config.secret_key_base = '0215763b3e626edd6876c475a5a9497695b315ddf1f80e2f4678b61e9d2c852b1a19225d76c28abe1290cf69e422837d462c2ac805af8596ca5d5e5ebd7a14c5'
